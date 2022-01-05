580ce85d0525b3f48e4451b5c898c0554d232ec0
Saw-jan <saw.jan.grg3e@gmail.com>
2022-01-05 21:19:52 +0545

    refactor db connection

--- Database/connection.js
+++ Database/connection.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
-const DB_URI = process.env.DATABASE_URI;
+const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_NAME = process.env.DB_NAME || "waveentrydatabase";
+
+const DB_URI = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}?retryWrites=true&w=majority`;
 
 const conn = () =>
   mongoose
